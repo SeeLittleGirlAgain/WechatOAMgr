1de4de4cb1ffcf78da25fb525cc0e3e96f9c6198
SeeLittleGirlAgain <1309230548@qq.com>
2016-01-31 23:41:10 +0800

    first commit

---
+++ src/weixin/util/WeiXinConstants.java
@@ -0,0 +1,17 @@
+package weixin.util;
+/**
+ * 
+ * @author hzf
+ * @version 1.0
+ */
+public class WeiXinConstants {
+
+	/**
+	 * 登录用户账号信息，加载到session缓存中
+	 */
+	public static final String WEIXIN_ACCOUNT = "WEIXIN_ACCOUNT";
+	/**
+	 *微信商城用户登录是的openid
+	 */
+	public static final String USER_OPENID = "USER_OPENID";
+}
